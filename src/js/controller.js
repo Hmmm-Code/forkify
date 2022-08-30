bf115495e1efaa73e0581745eb5c5391a2c697ae
“Khan” <skcbskcb0@gmail.com>
2022-08-30 19:42:15 +0400

    setup html and sass components

---
+++ src/js/controller.js
@@ -0,0 +1,23 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const showRecipe = async () => {
+  try {
+    fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+  } catch (error) {
+    alert(error);
+  }
+};
